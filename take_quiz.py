416d7bb8b77b6f79734de2ec3c91ba1a1c410c18
Shahabal1 <shahab.alijaniii@gmail.com>
2024-10-26 22:10:56 +0330

    Update take_quiz.py

--- take_quiz.py
+++ take_quiz.py
@@ -6,7 +6,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///quiz.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'supersecretkey'
 db = SQLAlchemy(app)
-class Question(db.Model):
+class Quiz(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     category = db.Column(db.String(255), nullable=False)
     question = db.Column(db.String(255), nullable=False)
@@ -19,9 +19,9 @@ def create_tables():
     db.create_all()
     if not Question.query.first():
         sample_quizzes = [
-            Question(category="Category 1", question="Question 1?", answer="Answer 1", option_1="Option 1A", option_2="Option 1B", option_3="Option 1C"),
-            Question(category="Category 1", question="Question 2?", answer="Answer 2", option_1="Option 2A", option_2="Option 2B", option_3="Option 2C"),
-            Question(category="Category 2", question="Question 3?", answer="Answer 3", option_1="Option 3A", option_2="Option 3B", option_3="Option 3C"),
+            Quiz(category="Category 1", question="Question 1?", answer="Answer 1", option_1="Option 1A", option_2="Option 1B", option_3="Option 1C"),
+            Quiz(category="Category 1", question="Question 2?", answer="Answer 2", option_1="Option 2A", option_2="Option 2B", option_3="Option 2C"),
+            Quiz(category="Category 2", question="Question 3?", answer="Answer 3", option_1="Option 3A", option_2="Option 3B", option_3="Option 3C"),
         ]
         db.session.bulk_save_objects(sample_quizzes)
         db.session.commit()
@@ -29,7 +29,7 @@ def create_tables():
 def take_quiz(category):
     quizzes = Quiz.query.filter_by(category=category).all()
     if not quizzes:
-        return jsonify({'message': 'No questions available in this category.'}), 404
+        return jsonify({'message': 'no ques dar in category.'}), 404
     selected_quizzes = random.sample(quizzes, min(len(quizzes), 5))
     return jsonify([{'id': q.id, 'question': q.question, 'options': [q.option_1, q.option_2, q.option_3]} for q in selected_quizzes])
 @app.route('/submit_quiz', methods=['POST'])
